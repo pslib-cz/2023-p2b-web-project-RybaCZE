09a9d384ccbb11271a04640d7836d473aeb1cbc2
RybaCZE <dominus.cz99@gmail.com>
2024-03-17 10:39:34 +0100

    Refactor code to improve performance and readability

---
+++ js/main.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.getElementById("navbar");
+  const navbarToggle = navbar.querySelector(".navbar-toggle");
+
+  function openMobileNavbar() {
+    navbar.classList.add("opened");
+    navbarToggle.setAttribute("aria-expanded", "true");
+  }
+
+  function closeMobileNavbar() {
+    navbar.classList.remove("opened");
+    navbarToggle.setAttribute("aria-expanded", "false");
+  }
+
+  navbarToggle.addEventListener("click", () => {
+    if (navbar.classList.contains("opened")) {
+      closeMobileNavbar();
+    } else {
+      openMobileNavbar();
+    }
+  });
+
+  const navbarMenu = navbar.querySelector("#navbar-menu");
+  const navbarLinks = navbarMenu.querySelectorAll(".navbar-link");
+
+  navbarLinks.forEach((link) => {
+    link.addEventListener("click", closeMobileNavbar);
+  });
+
+  const navbarLinksContainer = navbar.querySelector(".navbar-links");
+
+  if (navbarMenu && navbarLinksContainer) {
+    navbarLinksContainer.addEventListener("click", (clickEvent) => {
+      clickEvent.stopPropagation();
+    });
+
+    navbarMenu.addEventListener("click", closeMobileNavbar);
+  } else {
+    console.error("Navbar menu or links container not found.");
+  }
+});
+
+var currentYear = new Date().getFullYear();
+document.getElementById("year").textContent = currentYear;
